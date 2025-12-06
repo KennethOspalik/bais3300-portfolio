478dbad9c36aacb8ce9f183b0df28af190f5219b
KennethOspalik <kospalik@uiowa.edu>
2025-12-05 20:56:05 -0600

    fix3

--- assets/js/cookie-popup.js
+++ assets/js/cookie-popup.js
@@ -1,16 +1,26 @@
-// cookie-popup.js
+// cookie-popup.js  (keep this file in the SAME folder as index.html)
 
-const popup = document.getElementById("cookie-popup");
-const button = document.getElementById("accept-cookies");
+if (localStorage.getItem("cookie_accepted") === "true") {
+    // User already accepted – don't show it again
+} else {
+    fetch("cookie-popup.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("beforeend", data);
 
-if (!localStorage.getItem("cookie_accepted")) {
-    popup.style.display = "block";
+            const popup = document.getElementById("cookie-popup");
+            const button = document.getElementById("accept-cookies");
+
+            // Show popup
+            popup.style.display = "block";
+
+            // When user clicks Accept
+            button.addEventListener("click", () => {
+                localStorage.setItem("cookie_accepted", "true");
+                popup.style.display = "none";
+            });
+        });
 }
-
-button.addEventListener("click", () => {
-    localStorage.setItem("cookie_accepted", "true");
-    popup.style.display = "none";
-});
 
 
 
